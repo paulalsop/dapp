692385c222675819ed180eb783f4d1576847409d
fengfengei <14564815+fengfengei@user.noreply.gitee.com>
2024-08-21 18:48:39 +0800

    aaa4

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ export default createStore({
   state: {
     wbnb: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     USDT: '0x55d398326f99059fF775485246999027B3197955',
-    DP: ' 0x21dfe97101717ed7f562da5D1Ccbceef8fef33c3',
+    DP: '0x21dfe97101717ed7f562da5D1Ccbceef8fef33c3',
     DBTCoinNew: '0x6A47050B02E565D3a1562805E5D089eAA6e1DA8C',
     MintDBTC: '0x8F1832c385E1B26afbC970B1760cF9c2570C639e',
     lianjeshow:false
