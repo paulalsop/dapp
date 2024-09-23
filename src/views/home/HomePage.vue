050118d040e51c5b713dfd6934f5ecbdf3e7ef9d
paulalsop <102955363+paulalsop@users.noreply.github.com>
2024-09-23 10:07:58 +0800

    Update HomePage.vue
    update

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -457,7 +457,7 @@ async function deposit() {
         from: localStorage.getItem('address'),
         value: isnum,
         gasPrice: 3100000000,
-        gasLimit: 2300000
+        gasLimit: 7300000
       });
       allshow.value = false
       showSuccessToast(t('home.home43'));
@@ -467,7 +467,7 @@ async function deposit() {
       let aaaa = await contract1.methods.stakingCoins(bontarr.value[gommm.value].address, isnum).send({
         from: localStorage.getItem('address'),
         gasPrice: 3100000000,
-        gasLimit: 2300000
+        gasLimit: 7300000
       });
       allshow.value = false
       showSuccessToast(t('home.home43'));
